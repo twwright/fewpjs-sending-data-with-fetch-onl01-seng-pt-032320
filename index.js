c95bce89dd02d1198373616498a3c35b6ee28dbe
Thomas Wright <twwright@gmail.com>
2020-10-17 19:05:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    
+}
+
+
+let formData = {
+  dogName: "Byron",
+  dogBreed: "Poodle"
+};
+ 
+let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+ 
+fetch("http://localhost:3000/dogs", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  });
