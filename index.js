dab8c89e2aec899846f06e885ff6e3c9c266a68f
twwright <twwright@gmail.com>
2020-10-24 17:53:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+function submitData(name, email) {
+  let formData = {
+    name,
+    email
+  }
+  
+  let formObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  }
+
+  return fetch("http://localhost:3000/users", formObj)
+    .then((resp) => resp.json())
+    .then((obj) => document.body.innerHTML = obj.id)
+    .catch((error) => document.body.innerHTML = error.message)
+}
